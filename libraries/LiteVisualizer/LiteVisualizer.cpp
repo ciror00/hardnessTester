c97c3931cda50aecdb6fcc065cb6cc0ea329c230
ciror00 <ciror00@gmail.com>
2020-08-27 00:14:02 -0300

    Se agregan metodos nuevos

--- libraries/LiteVisualizer/LiteVisualizer.cpp
+++ libraries/LiteVisualizer/LiteVisualizer.cpp
@@ -2,6 +2,37 @@
 
 void LiteVisualizer::begin(){
   this->lcd.init();
+  this->lcd.createChar(0, card_ok);
+  this->lcd.createChar(1, global);
+  this->lcd.createChar(2, error);
+  this->lcd.createChar(3, arrow);
+}
+
+void LiteVisualizer::_showSettings(bool _sd_status, bool _gps_status){
+  byte file = 0;
+  this->lcd.clear();
+  // Mensaje de SD por apntalla
+  this->lcd.setCursor(0, file);
+  if(_sd_status){
+    this->lcd.write(0);
+    this->lcd.scrollDisplayRight();
+    this->lcd.print(": OK ");
+  }else{
+    this->lcd.write(1);
+    this->lcd.scrollDisplayRight();
+    this->lcd.print(":ERROR");
+  }
+  this->lcd.setCursor(6, file);
+  // Mensaje de GPS por pantalla
+  if(_gps_status){
+    this->lcd.write(1);
+    this->lcd.scrollDisplayRight();
+    this->lcd.print(": OK ");
+  }else{
+    this->lcd.write(2);
+    this->lcd.scrollDisplayRight();
+    this->lcd.print(":ERROR");
+  }
 }
 
 bool LiteVisualizer::switcher(bool n){
@@ -14,48 +45,19 @@ bool LiteVisualizer::switcher(bool n){
   }
 }
 
-void LiteVisualizer::showSettings(bool sd_status, bool gps_status){
-  byte file = 0;
-  this->lcd.clear();
-  this->lcd.createChar(0, card_ok);
-  this->lcd.createChar(1, global);
-  // Mensaje de SD por apntalla
-  this->lcd.setCursor(0, file);
-  if(sd_status){
-    this->lcd.write(0);
-    this->lcd.scrollDisplayRight();
-    this->lcd.print(": OK ");
-  }else{
-    this->lcd.write(1);
-    this->lcd.scrollDisplayRight();
-    this->lcd.print(":ERROR");
-  }
-  this->lcd.setCursor(8, file);
-  // Mensaje de GPS por pantalla
-  if(gps_status){
-    this->lcd.write(1);
-    this->lcd.scrollDisplayRight();
-    this->lcd.print(": OK ");
-  }else{
-    this->lcd.write(2);
-    this->lcd.scrollDisplayRight();
-    this->lcd.print(":ERROR");
-  }
-}
-
-void LiteVisualizer::showMessage(String text, String header, String footer, bool clear){
+void LiteVisualizer::showMessage(String header, String text, String footer, bool clear){
   if(clear)this->lcd.clear();
-  this->lcd.setCursor(1, 1);
+  this->lcd.setCursor(0, 1);
   this->lcd.print(header);
-  this->lcd.setCursor(1, 2);
+  this->lcd.setCursor(0, 2);
   this->lcd.print(text);
-  this->lcd.setCursor(1, 3);
+  this->lcd.setCursor(0, 3);
   this->lcd.print(footer);
 }
 
 void LiteVisualizer::showMeasure(int line, String label, String value, String unit, bool clear){
   if(clear)this->lcd.clear();
-  if(line == 1 || line == 2){
+  if(line == 2 || line == 3){
     this->lcd.setCursor(1, line);
   }
   this->lcd.print(label);
@@ -67,4 +69,40 @@ void LiteVisualizer::showMeasure(int line, String label, String value, String un
 
 void LiteVisualizer::reset(){
   this->lcd.clear();
+}
+
+void LiteVisualizer::home(bool sd_status, bool gps_status){
+  this->_showSettings(sd_status, gps_status);
+  this->lcd.setCursor(0, 3);
+  this->lcd.write(3);
+  this->lcd.setCursor(19, 3);
+  this->lcd.write(3);
+  this->showMessage(" ", "Listo para Medir", " ", false);
+}
+
+void LiteVisualizer::detail(bool sd_status, bool gps_status, bool saving, String number){
+  char buff[10];
+  this->lcd.clear();
+  this->_showSettings(sd_status, gps_status);
+  this->lcd.setCursor(0, 1);
+  if(saving){
+    sprintf(buff, "Guardado SD N°%04d", number);
+  }else{
+    sprintf(buff, "No SD. No Guardado");
+  }
+}
+
+void LiteVisualizer::summary(String max, String averange, String distance, String regiter){
+  // Lado izquierdo
+  this->lcd.setCursor(0, 2);
+  this->lcd.print(averange);
+  this->lcd.setCursor(4, 2);
+  this->lcd.print("Kg Fmax:");
+  this->lcd.print(max);
+  // Lado derecho
+  this->lcd.setCursor(0, 3);
+  this->lcd.print(distance);
+  this->lcd.setCursor(4, 2);
+  this->lcd.print("cm Dmax:");
+  this->lcd.print(regiter);
 }
--- libraries/LiteVisualizer/LiteVisualizer.h
+++ libraries/LiteVisualizer/LiteVisualizer.h
@@ -8,14 +8,18 @@
 
 class LiteVisualizer{
   LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 20, 4); // columna || fila
+  void _showSettings(bool _sd_status, bool _gps_status);
 
   public:
     LiteVisualizer() = default;
     void begin();
     bool switcher(bool n);
-    void showSettings(bool sd_status, bool gps_status);
-    void showMessage(String text, String header = " ", String footer = " ", bool clear = true);
+    //void showSettings(bool sd_status, bool gps_status);
+    void showMessage( String header, String text, String footer, bool clear = true);
     void showMeasure(int line, String label, String value, String unit, bool clear = true);
+    void home(bool sd_status, bool gps_status);
+    void detail(bool _sd_status, bool _gps_status, bool saving, String number);
+    void summary(String max, String averange, String distance, String regiter);
     void reset();
     ~LiteVisualizer() = default;
 };
