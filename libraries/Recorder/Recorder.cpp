80d029611143354a3263e6b71b07cb7fbcc098a5
Ciro Edgardo Romero <ciror00@gmail.com>
2020-08-28 17:32:35 -0300

    Se quita registro de horas por linea

--- libraries/Recorder/Recorder.cpp
+++ libraries/Recorder/Recorder.cpp
@@ -96,10 +96,10 @@ bool Recorder::saveRegistry(int numb, ...){
 	// Primero se chequea que el archivo exista
 	if(SD.exists(buffer)){
 		this->registry = SD.open(buffer, FILE_WRITE);
-		String data;
-		sprintf(buffer, "%02d/%02d/%04d;", this->date.day(), this->date.month(), this->date.year());
+		String data = ";";
+		//sprintf(buffer, "%02d/%02d/%04d;", this->date.day(), this->date.month(), this->date.year());
 		data += buffer;
-		sprintf(buffer, "%02d:%02d:%02d;", this->date.hour(), this->date.minute(), this->date.second());
+		//sprintf(buffer, "%02d:%02d:%02d;", this->date.hour(), this->date.minute(), this->date.second());
 		data += buffer;
 		// Se recorren todos los datos que se quieren ingresar en el archivo
 		va_list ap;
