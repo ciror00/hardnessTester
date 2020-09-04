945a4938cd71c711b4a33022a8ac094ecc1c8027
Ciro Edgardo Romero <ciror00@gmail.com>
2020-09-04 13:14:32 -0300

    Configuracion del owner

--- schemes/ground/settings.h
+++ schemes/ground/settings.h
@@ -2,9 +2,9 @@
 #define LOGS 0
 
 // Declaraciones de la configuracion
-const long sensibility = 3; // Valor minimo de fuerza para tomar un registro [kg]
-const long tolerance = 1; // Corrimiento minimo de profundidad [cm]
-const byte stabilizer = 3; // Cantidad de muestras antes de devolver una medicion
+const long sensibility = 2; // Valor minimo de fuerza para tomar un registro [kg]
+const long tolerance = 0; // Corrimiento minimo de profundidad [cm]
+const byte stabilizer = 2; // Cantidad de muestras antes de devolver una medicion
 const float scale = 62000; // Escala para configuracion por defecto
 
 // Tiempos parametrizables
