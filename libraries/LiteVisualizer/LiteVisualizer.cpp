218dbdbb1bba6ea3dd54b469241895b2766e6b2b
Ciro Edgardo Romero <ciror00@gmail.com>
2020-09-03 22:47:00 -0300

    Se comentan los corrimientos

--- libraries/LiteVisualizer/LiteVisualizer.cpp
+++ libraries/LiteVisualizer/LiteVisualizer.cpp
@@ -44,22 +44,22 @@ void LiteVisualizer::showHeader(bool sd_status, bool gps_status, bool clear){
   //this->lcd.scrollDisplayLeft();
   if(gps_status){
     this->lcd.write(0);
-    this->lcd.scrollDisplayRight();
+    //this->lcd.scrollDisplayRight();
     this->lcd.print("GPS.Ok");
   }else{
     this->lcd.write(0);
-    this->lcd.scrollDisplayRight();
+    //this->lcd.scrollDisplayRight();
     this->lcd.print("NO.GPS");
   }
   this->lcd.setCursor(8, file);
   // Mensaje de SD por pantalla
   if(sd_status){
     this->lcd.write(1);
-    this->lcd.scrollDisplayRight();
+    //this->lcd.scrollDisplayRight();
     this->lcd.print("SD.OK");
   }else{
     this->lcd.write(1);
-    this->lcd.scrollDisplayRight();
+    //this->lcd.scrollDisplayRight();
     this->lcd.print("NO.SD");
   }
 }
@@ -90,7 +90,7 @@ void LiteVisualizer::showMeasure(int line, String label, String value, String un
     this->lcd.setCursor(1, line);
   }
   this->lcd.print(label);
-  this->lcd.scrollDisplayRight();
+  //this->lcd.scrollDisplayRight();
   this->lcd.print(value);
   this->lcd.setCursor(10, line);
   this->lcd.print(unit);
@@ -130,7 +130,7 @@ void LiteVisualizer::summary(String averange, String max, String distance, Strin
   byte column[] = {0};
   byte file[] = {2,3};
   String fileOne = "Fp: " + averange + "Fm:" + max;
-  String fileTwo = "Dm: " + distance + "   Dfm:" + regiter;
+  String fileTwo = "Dm: " + distance + "    Dfm:" + regiter;
   //this->lcd.scrollDisplayLeft(); 
   //this->lcd.scrollDisplayLeft(); 
   this->lcd.setCursor(column[0], file[0]);
